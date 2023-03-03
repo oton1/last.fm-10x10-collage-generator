6b9d22b323a8d2d522ba74d4f7818314c79ae245
oton1 <otoniotoni22@gmail.com>
2023-03-03 12:32:29 -0300

    Creation of ImageDraw function to create the png

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from io import BytesIO
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import discord
 import os
 from dotenv import load_dotenv
@@ -10,12 +10,11 @@ load_dotenv()
 intent = discord.Intents.default()
 intent.members = True
 intent.message_content = True
-client = discord.Client(intent=intent)
+client = discord.Client(intents=intent)
 
 # Replace with your own last.fm username and API key
-username = "your_username"
-api_key = "your_api_key"
-
+username = 'insert your username'
+api_key = 'insert your last.fm API Key'
 # Set the time period to the last month
 time_period = "1month"
 
@@ -23,11 +22,13 @@ time_period = "1month"
 response = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user={username}&api_key={api_key}&period={time_period}&limit=100&format=json")
 
 # Extract the top artists from the API response
-top_artists = response.json()["topartists"]["artist"]
-
+top_artists = response.json()['topartists']['artist']
 # Create a new image with a size of 1000x1000 pixels
-image = Image.new("RGB", (1000, 1000), (255, 255, 255))
-
+image = Image.new("RGB", (1000, 1000), (0, 0, 0))
+# Creation of the png 
+draw = ImageDraw.Draw(image)
+font_size = 20
+font = ImageFont.truetype('arial.ttf', font_size)
 # Loop through the top artists and paste their images onto the image
 for i, artist in enumerate(top_artists):
     # Make a request to the last.fm API to get the artist's image
@@ -45,8 +46,8 @@ for i, artist in enumerate(top_artists):
 # Save the image to disk
 image.save("lastfm_collage.png")
 
+#Work in progress: implement the function in a discord bot
 @client.event
 async def on_ready():
     print("10x10 Collage bot ready for use")
-    
-client.run(os.getenv('discord token'))
+
